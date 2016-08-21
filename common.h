7623b5820247ba9c3d7532af29de6ad7641565fd
John Lindgren <john.lindgren@aol.com>
2016-08-21 12:30:47 -0400

    Relicense under BSD 2-clause.

--- common.h
+++ common.h
@@ -1,17 +1,19 @@
 // AEq -- Equalizer plugin for ALSA
 // Copyright 2010 John Lindgren <john.lindgren@aol.com>
 //
-// This program is free software: you can redistribute it and/or modify it under
-// the terms of the GNU General Public License as published by the Free Software
-// Foundation, version 2.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
 //
-// This program is distributed in the hope that it will be useful, but without
-// any warranty; without even the implied warranty of merchantability or fitness
-// for a particular purpose.  See the GNU General Public License for more
-// details.
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions, and the following disclaimer.
 //
-// You should have received a copy of the GNU General Public License along with
-// this program.  If not, see <http://www.gnu.org/licenses/>.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions, and the following disclaimer in the documentation
+//    provided with the distribution.
+//
+// This software is provided "as is" and without any warranty, express or
+// implied. In no event shall the authors be liable for any damages arising from
+// the use of this software.
 
 #include <errno.h>
 #include <stdio.h>
